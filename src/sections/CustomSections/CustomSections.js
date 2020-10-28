653136d102dbf9284ad46f330050954be9da4abe
Fawad Ali <m.fawaadali98@gmail.com>
2020-10-28 21:15:51 +0500

    Minor code improvements

--- src/sections/CustomSections/CustomSections.js
+++ src/sections/CustomSections/CustomSections.js
@@ -7,14 +7,17 @@ class CustomSections extends React.Component {
     render() {
         return (
             <div>
-                {CustomSectionsConfig.map((section, index) => {
-                    return (
-                        <Section key={'section-' + index} ref={this.props.sectionRefs[section.name]} sectionHeader={section.name} headerIcon={section.headerIcon}
-                            extraClass={section.extraClass ? section.extraClass : ''}>
-                            {section.content}
-                        </Section>
-                    )
-                })}
+                {CustomSectionsConfig.map((section, index) => (
+                    <Section
+                        key={'section-' + index}
+                        ref={this.props.sectionRefs[section.name]}
+                        sectionHeader={section.name}
+                        headerIcon={section.headerIcon}
+                        extraClass={section.extraClass ? section.extraClass : ''}
+                    >
+                        {section.content}
+                    </Section>
+                ))}
             </div>
         );
     }
